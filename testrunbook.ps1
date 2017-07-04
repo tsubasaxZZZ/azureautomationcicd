c957c3ee86ecdefba45a394bf1f2cc6982c20a31
Tsubasa Nomura <tsunomur@microsoft.com>
2017-07-04 18:16:31 +0900

    Initial updaste

---
+++ testrunbook.ps1
@@ -0,0 +1,35 @@
+#$automationAccountName =  "tsunomur0704automation"
+#$runbookName = "testrunbook"
+#$scriptPath = "C:\Users\tsunomur\OneDrive - Microsoft\work\Work Folders\200_Work\20170630_英検\azureautomationcicd\testrunbook.ps1"
+#$RGName = "tsunomur0704automation"
+#Import-AzureRMAutomationRunbook -Name $runbookName -Path $scriptPath -ResourceGroupName $RGName -AutomationAccountName $automationAccountName -Type PowerShellWorkflow 
+
+workflow testrunbook {
+    $msg = ((Get-Date).AddHours(+9)).ToString("yyyyMMdd-HHmmss") + " #Start testrunbook"
+    Write-Output $msg
+    
+    $connectionName = "AzureRunAsConnection"
+    try
+    {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+        "Logging in to Azure..."
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    }
+    catch {
+        if (!$servicePrincipalConnection)
+        {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        } else{
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+    Get-AzureRmVM
+}
